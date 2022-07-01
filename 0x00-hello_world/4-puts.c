f358074abddbd8ecdca1f3f6cedfe91fae8b4b27
Nathan Omeri <magembenath@gmail.com>
2022-06-30 18:36:41 -0700

    print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry and exit of the program
+ * @my: an array of characters that we want to print
+ *
+ * Description: it prints a given line of code
+ * Returnn: it returns 0 since it is an int
+ */
+int main(void)
+{
+	char my[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my);
+
+	return (0);
+}
