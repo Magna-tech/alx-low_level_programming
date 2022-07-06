b459cd5ff7158c8edbe448093cf3a32422e5dc09
Nathan Omeri <magembenath@gmail.com>
2022-07-05 19:56:10 -0700

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first value
+ * @b: second value
+ *
+ * Return: answer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ *
+ * Return: no value
+ */
+void times_table(void)
+{
+	int x, y;
+}
