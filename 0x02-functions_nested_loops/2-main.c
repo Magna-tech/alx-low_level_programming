9c9e26ed32736bb8467e09b9ce8a0eb1e8696aaa
Nathan Omeri <magembenath@gmail.com>
2022-07-05 18:09:46 -0700

    print 10 times alphas

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entry of program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ *
+ * Return: no value
+ */
+void print_alphabet_x10(void)
+{
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+}
