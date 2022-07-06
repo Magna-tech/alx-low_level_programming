105c3e6a2501f5cefea53c0b9057e64c5ce15c1d
Nathan Omeri <magembenath@gmail.com>
2022-07-05 18:15:51 -0700

    print alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
-	print_alphabet();
+	int a;
+	char b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+			_putchar(b);
+		_putchar('\n');
+	}
 }
