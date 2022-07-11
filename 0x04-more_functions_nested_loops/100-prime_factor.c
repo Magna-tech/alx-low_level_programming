9b46cf85548f489d5b27dc320fd3b6bd6356c9b0
Nathan Omeri <magembenath@gmail.com>
2022-07-10 17:36:26 -0700

    advanced tasks trial

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print prime factors of a number
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	long int num = 612852475143;
+	int p;
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: number to be printed
+ *
+ * Return: no value
+ */
+
+void print_number(int n)
+{
+	_putchar((n % (n + 1)) + '0';
+}
