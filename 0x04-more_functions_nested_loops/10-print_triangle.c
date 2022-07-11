51655919943397695e6cd05637b4e7999c0e8794
Nathan Omeri <magembenath@gmail.com>
2022-07-10 17:06:45 -0700

    Print triangle fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ void print_triangle(int size)
 		{
 			for (k = size; k >= i; k--)
 				_putchar(' ');
-			for (j = 1; j <= i; j++)
 			{
-				_putchar('#');
+				for (j = 1; j <= i; j++)
+					_putchar('#');
 			}
 			_putchar('\n');
 		}
