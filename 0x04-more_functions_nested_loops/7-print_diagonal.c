3f0ee20a86f1719a9e4e11a8079196e0042c88e7
Nathan Omeri <magembenath@gmail.com>
2022-07-10 17:22:58 -0700

    print diagonal backslashes

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,12 +10,18 @@ void print_diagonal(int n)
 {
 	int i, k;
 
-	for (i = n; i > 0; i--)
-   	{
-		for (k = 1; k <= n; k++)
-			_putchar(' ');
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (k = 0; k < i; k++)
+				_putchar(' ');
 		_putchar('\\');
-		n--;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
