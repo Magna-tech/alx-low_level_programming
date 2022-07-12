377f8f75b7dadbb867bd012cd999475f70f0fd2d
Nathan Omeri <magembenath@gmail.com>
2022-07-11 19:10:39 -0700

    convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the integer value of the string
+ */
+
+int _atoi(char *s)
+{
+	int p = 1;
+	unsigned int n = 0;
+
+	do {
+		if (*s == '-')
+			p = p * -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else
+			break;
+	} while (*s++);
+
+	return (p * n);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  generate random passcodes
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int rand;
+
+	return (0);
+}
