bd29dd577e91cf48be4d13e1d3343ecdf4080a05
Nathan Omeri <magembenath@gmail.com>
2022-07-11 19:19:58 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,7 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
  * main -  generate random passcodes
@@ -9,7 +11,17 @@
 
 int main(void)
 {
-	int rand;
+	int sum;
+	char a;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum +=  c;
+		_putchar(c);
+	}
+	_putchar(2772 - sum);
 
 	return (0);
 }
