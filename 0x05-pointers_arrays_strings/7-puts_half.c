ebf99bf6434caba475c1b48570daacd26430ceb0
Nathan Omeri <magembenath@gmail.com>
2022-07-11 17:32:59 -0700

    print half the elements of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the characters of a string
+ * @str: the string to be halved
+ *
+ * Return: no value
+ */
+
+void puts_half(char *str)
+{
+	int len, i;
+
+/*We first get the length of the string */
+	while (str[len] != '\0')
+		len++;
+	while (str[i] <= (len / 2))
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
