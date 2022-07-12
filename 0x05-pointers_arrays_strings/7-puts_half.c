3dbe1641fbdeafdfe4610b6570578b67854f3403
Nathan Omeri <magembenath@gmail.com>
2022-07-11 18:01:33 -0700

    print half fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@
 
 void puts_half(char *str)
 {
-	int len = 0, i = 0;
+	int len = 0, i;
 
 /*We first get the length of the string */
 	while (str[len] != '\0')
@@ -22,7 +22,7 @@ void puts_half(char *str)
 	else
 	{
 		for (i = (len - 1) / 2; str[i] != '\0'; i++)
-			_putchar(str[i];
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
