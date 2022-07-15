b41de40e98fb357b706a61619bc32b9a9aa58e75
Nathan Omeri <magembenath@gmail.com>
2022-07-14 19:59:40 -0700

    creation of files

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer
+ * @size_r: buffer size of r
+ *
+ * Return: pointer to the result
+ * 0 if result cannot be stored in r
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	/*code goes here*/
+	return (result);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer to a buffer
+ * @size: size of the buffer pointed to
+ *
+ * Return: no value
+ */
+
+void print_buffer(char *b, int size)
+{
+	/*code goes her*/
+}
