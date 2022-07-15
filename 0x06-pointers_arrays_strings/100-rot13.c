89e8b72fb0e2e1f03acea309f338080ed2f8cbfb
Nathan Omeri <magembenath@gmail.com>
2022-07-14 19:34:12 -0700

    print number using _putchar

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes string using rot13
+ * @s: string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	/*code goes here*/
+}
