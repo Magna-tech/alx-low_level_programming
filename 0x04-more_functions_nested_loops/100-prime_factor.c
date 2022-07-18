2bcb3d18cbe47944d23c266f00c87820ab6301fb
Nathan Omeri <magembenath@gmail.com>
2022-07-17 18:18:21 -0700

    Find largest prime factor of a given number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,5 +11,15 @@ int main(void)
 	long int num = 612852475143;
 	int p;
 
+	for (p = 2; p <= sqrt(num); p++)
+	{
+		if (num % p == 0)
+		{
+			num = num / p;
+			p = 1;
+		}
+	}
+	printf("%ld\n", num);
+
 	return (0);
 }
