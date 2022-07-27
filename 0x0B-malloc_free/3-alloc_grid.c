de696c1ea173d06c4e4d4b2361cece1b45ca7918
Nathan Omeri <magembenath@gmail.com>
2022-07-26 18:08:25 -0700

    create a 2D array and return a pointer to it

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,5 +11,23 @@
 
 int **alloc_grid(int width, int height)
 {
-	return ('\0');
+	int **grid, i, j;
+
+	if (width < 1 || height < 1)
+		return ('\0');
+	grid = malloc(height * sizeof(int));
+	if (grid == NULL)
+		return ('\0');
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+			return ('\0');
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
 }
