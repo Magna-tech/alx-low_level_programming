73dedee29659e0821d8f567868c7f2bbb98a6874
Nathan Omeri <magembenath@gmail.com>
2022-07-28 18:49:32 -0700

    Allocate memory using malloc and return pointer to memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
@@ -10,11 +11,11 @@
 
 void *malloc_checked(unsigned int b)
 {
-	int i;
+	int *i;
 
 	i = malloc(b);
 	if (i == NULL)
 		exit(98);
 	else
-		printf("%x\n", &i);
+		return (i);
 }
