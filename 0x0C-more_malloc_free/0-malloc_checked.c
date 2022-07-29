449719c2ff05f6a0e4066582f3b540657c11d072
Nathan Omeri <magembenath@gmail.com>
2022-07-28 18:25:56 -0700

    malloc trial

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to be allocated
+ *
+ * Description: return a pointer to the allocated memory
+ * Return: no value
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	else
+		printf("%x\n", &i);
+}
