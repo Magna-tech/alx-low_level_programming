b65226da5d20648e00199ff8d9f1508a6a5e7ad3
Nathan Omeri <magembenath@gmail.com>
2022-12-07 16:57:06 -0800

    Island Perimeter skeleton

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function that calculates the perimeter of a given grid"""
+
+
+def island_perimeter(grid):
+    """
+    Find the length and width of land area
+    0 means water and 1 means land
+    Add them and multiply by two
+    Return the perimeter
+    """
+    return 1
