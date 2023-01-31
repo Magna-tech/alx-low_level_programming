c5be8009eac8462d47a8aa8f2b4b88b0bad54d34
Nathan Omeri <magembenath@gmail.com>
2023-01-30 17:55:51 -0800

    Template for binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array
+ * @array: the given array
+ * @size: number of elements in array
+ * @value: the value to search for
+ *
+ * Return:index where the value is located or -1 otherwise
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+}
