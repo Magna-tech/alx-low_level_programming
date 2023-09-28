a0294fb30ed56fbe1d53fbd78f903b9dd62018b2
Nathan Omeri <magembenath@gmail.com>
2023-09-28 14:36:39 +0000

    Template and test file for project 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename: name of the file
+ * @text_content: the content of the file created
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	return (-1);
+}
