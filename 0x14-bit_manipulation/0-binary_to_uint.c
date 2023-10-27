8fc578a014d47d73e13d8c7c93da8c8df42212bf
Nathan Omeri <magembenath@gmail.com>
2023-10-27 02:49:02 +0000

    Changed the bitwise operator to && also fixed the if statement

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,9 +14,9 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-	while (i > 0)
+	while (b[i] != '\0')
 	{
-		if (b[i] != 0 || b[i] != 1)
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
