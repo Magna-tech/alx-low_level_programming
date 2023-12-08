8065f0bb357c24e0d23cbe9e429561b698d6f18f
Nathan Omeri <magembenath@gmail.com>
2023-12-08 02:58:50 +0000

    Inserting node template

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a node at a given index
+ * @head: pointer to the start of the list
+ * @idx: index where the new node should be added
+ * @n: new node
+ *
+ * Return: address of new node or NULL on failure
+ */
+listint_t *insert_dnodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	(void)idx;
+	(void)n;
+
+	if (*head == NULL)
+		return (NULL);
+	else
+		return (NULL);
+}
