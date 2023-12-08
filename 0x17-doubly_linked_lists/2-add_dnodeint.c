0b5ca5123908c6bbc5a5bf646f4b64614ea168b6
Nathan Omeri <magembenath@gmail.com>
2023-12-08 00:59:56 +0000

    Added the pointer to the prev node for both the new node and the previous head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	new->prev = NULL;
 	new->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new;
 
 	*head = new;
 
