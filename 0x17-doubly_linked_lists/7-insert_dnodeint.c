f81b812d1a8880bae32aee905af1cf5a28701090
Nathan Omeri <magembenath@gmail.com>
2023-12-08 03:02:01 +0000

    Function update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,13 +2,13 @@
 
 /**
  * insert_dnodeint_at_index - inserts a node at a given index
- * @head: pointer to the start of the list
+ * @h: pointer to the start of the list
  * @idx: index where the new node should be added
  * @n: new node
  *
  * Return: address of new node or NULL on failure
  */
-listint_t *insert_dnodeint_at_index(listint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	(void)idx;
 	(void)n;
