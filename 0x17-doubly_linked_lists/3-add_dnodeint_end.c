54efaa4283948e09d7249d043aeb01dbbc940b18
Nathan Omeri <magembenath@gmail.com>
2023-12-08 01:07:16 +0000

    Linked the new node properly

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,12 +19,16 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new->next = NULL;
 
 	if (*head == NULL)
+	{
+		new->prev = NULL;
 		*head = new;
+	}
 	else
 	{
 		last = *head;
 		while (last->next != NULL)
 			last = last->next;
+		new->prev = last;
 		last->next = new;
 	}
 	return (*head);
