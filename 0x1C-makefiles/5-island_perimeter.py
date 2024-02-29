13e38e9d47edac0ee88689befa0d00f665b9b93e
Nathan Omeri <magembenath@gmail.com>
2024-02-29 13:48:09 +0000

    Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,4 +9,20 @@ def island_perimeter(grid):
     Add them and multiply by two
     Return the perimeter
     """
-    return 1
+    perimeter = 0
+
+    if not grid:
+        return perimeter
+
+    row = len(grid)
+    col = len(grid[0])
+
+    for i in range(row):
+        for j in range(col):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
